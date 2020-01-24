2975b08ec0dea55ae7b51779d249918fabf11de4
Sergey Kalashnikov <51378932+SergeyK-hs@users.noreply.github.com>
2020-01-24 07:12:31 +0400

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -1,11 +1,13 @@
 file = open('pushkin.txt', 'r')
-f = file.read().replace("\n"," ").replace("\t","").replace("\xa0\xa0\xa0\xa0","").replace(",","").replace(".","").split(" ")
+f = file.read().replace("\n"," ").replace("\t","").replace("\xa0\xa0\xa0\xa0","").replace(",","").replace(".","").upper().split(" ")
 dict = {}
 for word in f:
-    if dict.get(word) != None:
-        dict[word]+=1
-    else:
-        dict.update([(word,1)])
-print(dict)
+   if dict.get(word):    
+       dict[word]+=1
+   else:
+       dict[word]=1
+dict={k: v for k, v in sorted(dict.items(), key=lambda item: item[1], reverse = True)[:60]}
+for key, value in dict.items():
+    print(key, ":", value)
 
 
